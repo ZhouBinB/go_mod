a99880da05c27003f07510cf55b488a3f0585740
zb <zb@sxpyw.com>
2023-01-24 13:50:17 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ZhouBinB/go_mod
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" 
+
+// say Hi to someone
+func SayHi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
